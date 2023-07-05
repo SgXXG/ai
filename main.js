0df5e67ce7dcffbd3331771b015b7e8aac26715a
Stanislav <72697394+SgXXG@users.noreply.github.com>
2023-07-05 13:25:34 +0300

    generating images

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import config from 'config'
 import { engine } from "express-handlebars";
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: config.get('OPENAI_KEY'),
+})
+const openai = new OpenAIApi(configuration)
 
 const app = express()
 
@@ -13,15 +19,27 @@ app.get('/', (_, res) => {
     res.render('index')
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
     const prompt = req.body.prompt
-    const size = req.body.size
+    const size = req.body.size ?? '512x512'
     const number = req.body.number ?? 1
 
-    console.log(prompt, size, number)
+    try {
+        const response = await openai.createImage({
+            prompt,
+            size,
+            n: Number(number)
+        })
 
-    // reload the page
-    res.render('index')
+        // reload the page
+        res.render('index', {
+            images: response.data.data,
+        })
+    } catch (e) {
+        res.render('index', {
+            error: e.message,
+        })
+    }
 })
 
 app.listen(3000, () => console.log('Server started on port 3000'))
